cdc182b5e84dd12ae411086084dacf03496abf1f
Ariane <ariane.fairlie@gmail.com>
2020-09-26 14:01:26 -0400

    nominations modal

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useReducer, useEffect} from 'react';
-import { StyleSheet, View, ScrollView, SafeAreaView, Alert } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, SafeAreaView, Alert, Modal } from 'react-native';
+import {BlurView} from 'expo-blur'
 import { Button, Icon } from 'react-native-elements';
 
 import formatSearchResults from './helpers/formatResults'
@@ -25,23 +26,21 @@ function reducer(state, action) {
         results[action.index] = action.movie
         return {...state, nominations: [...state.nominations, action.movie], results}
       } else {
+        let displayModal = false;
         // TO DO: send up an alert: you can only nominate 5 films!
         Alert.alert(
           "Oops!",
           "You can only nominate 5 films",
           [
             { text: "cancel", onPress: () => {}, style: 'cancel' },
-            {
-              text: "nominations",
-              onPress: () => {
-                // navigate to nominations
-              }
-            },
           ],
           { cancelable: false }
         )
         return state
       }
+    }
+    case 'TOGGLE_MODAL': {
+      return {...state, listModal: !state.listModal}
     }
     default: {
       throw new Error('no reducer action of that type')
@@ -54,12 +53,13 @@ export default function App() {
     results: [],
     nominations: [],
     intro: true,
+    listModal: false
   })
 
   useEffect(() => {
     setTimeout(() => {
       dispatch({ type: 'SET_RESULTS', results: [], setIntro: true })
-    }, 5000)
+    }, 4000)
   }, [])
 
   function handlePress(movie, index) {
@@ -68,36 +68,58 @@ export default function App() {
 
   return (
     <DismissKeyboard>
-    <SafeAreaView style={styles.container}>
-      <Logo/>
-      <View style={styles.actions}>
-      <Icon
-        raised
-        name='list'
-        type='font-awesome'
-        color='rgb(105,78,46)'
-        reverse
-      />
-      <Search dispatch={dispatch}/>
-      </View>
-      <ScrollView contentContainerStyle={styles.main}>
-      {state.intro ? <Intro/> 
-        : state.results.map((movie, i) => {
-        return (
-          <Button 
-            key={i} 
-            type='outline'
-            title={`${movie.title}, ${movie.year}`}
-            titleStyle={{color: `rgb(0,100,0)`, fontSize: 12}}
-            buttonStyle={styles.result}
-            onPress={() => handlePress(movie, i)}
-            disabled={movie.nominated}
-          />
-        )
-      })}
-      </ScrollView>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <Logo/>
+        <View style={styles.actions}>
+        <Icon
+          raised
+          name='list'
+          type='font-awesome'
+          color='rgb(105,78,46)'
+          reverse
+          onPress={() => dispatch({type: 'TOGGLE_MODAL'})}
+        />
+        <Search dispatch={dispatch}/>
+        </View>
+        <ScrollView contentContainerStyle={styles.main}>
+        {state.intro ? <Intro/> 
+          : state.results.map((movie, i) => {
+          return (
+            <Button 
+              key={i} 
+              type='outline'
+              title={`${movie.title}, ${movie.year}`}
+              titleStyle={{color: `rgb(0,100,0)`, fontSize: 12}}
+              buttonStyle={styles.result}
+              onPress={() => handlePress(movie, i)}
+              disabled={movie.nominated}
+            />
+          )
+        })}
+        </ScrollView>
+        <Modal transparent visible={state.listModal}>
+          <BlurView tint='dark' intensity={5} style={styles.modalBackground}>
+            <View style={styles.modal}>
+              <Text style={styles.modalTitle}>Your nominations...</Text>
+              {state.nominations.map((movie, i) => {
+                return (
+                  <Button 
+                    key={i} 
+                    type='outline'
+                    title={`${movie.title}, ${movie.year}`}
+                    titleStyle={{color: `rgb(0,100,0)`, fontSize: 12}}
+                    buttonStyle={styles.result}
+                    onPress={() => handlePress(movie, i)}
+                    disabled={movie.nominated}
+                  />
+                )
+              })}
+              <Button type='outline' title='close' onPress={() => dispatch({type: 'TOGGLE_MODAL'})}/>
+            </View>
+          </BlurView>
+        </Modal>
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </DismissKeyboard>
   );
 }
@@ -116,6 +138,26 @@ const styles = StyleSheet.create({
     minHeight: 120,
     marginTop: 10,
     paddingBottom: 20,
+  },
+  modalBackground: {
+    flex: 1,
+    display: `flex`,
+    justifyContent: `center`,
+    alignItems: `center`,
+  },
+  modal: {
+    height: 350,
+    width: 360,
+    borderRadius: 20,
+    backgroundColor: `white`,
+    padding: 20,
+  },
+  modalTitle: {
+    fontFamily: `Avenir-Heavy`,
+    fontSize: 20,
+    paddingLeft: 5,
+    color: `rgb(0,100,0)`,
+    marginBottom: 20,
   },
   actions: {
     display: `flex`,
